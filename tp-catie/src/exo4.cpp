000791bf966695959860504779e9227c7e11ee4a
VayBar <guillaumemara83@gmail.com>
2026-01-20 17:47:16 +0100

    ajout du raw catie

--- tp-catie/src/exo4.cpp
+++ tp-catie/src/exo4.cpp
@@ -1,13 +1,44 @@
 #include "exo4.hpp"
+#include <cstdint>
 
-void read_temperature(BME280 bme280){
-    printf("Il fait %fC\n", bme280.temperature());
+void read_temperature(BME280 bme280) {
+  printf("Il fait %fC\n", bme280.temperature());
 }
 
-void read_pressure(BME280 bme280){
-    printf("Il fait %fPa\n", bme280.pressure());
+void read_pressure(BME280 bme280) {
+  printf("Il fait %fPa\n", bme280.pressure());
 }
 
-void read_humidity(BME280 bme280){
-    printf("Il fait %f%\n", bme280.humidity());
+void read_humidity(BME280 bme280) {
+  printf("Il fait %f%\n", bme280.humidity());
+}
+
+void read_raw_temperature(I2C i2c) {
+  uint8_t data[3];
+  i2c.write(I2C_ADDRESS, TEMP_REG, 1);
+  i2c.read(I2C_ADDRESS, data, 3);
+
+  int32_t raw_temp = (data[0] << 16) | (data[1] << 8) | data[2];
+  float temp = (raw_temp / 100.0);
+  printf("Il fait %fC\n", temp);
+}
+
+void read_raw_pressure(I2C i2c) {
+  uint8_t data[3];
+  i2c.write(I2C_ADDRESS, PRESS_REG, 1);
+  i2c.read(I2C_ADDRESS, data, 3);
+
+  int32_t raw_press = (data[0] << 16) | (data[1] << 8) | data[2];
+  float press = (raw_press / 100.0);
+  printf("Il fait %fPa\n", press);
+}
+
+void read_raw_humidity(I2C i2c) {
+  uint8_t data[3];
+  i2c.write(I2C_ADDRESS, HUM_REG, 1);
+  i2c.read(I2C_ADDRESS, data, 3);
+
+  int32_t raw_hum = (data[0] << 16) | (data[1] << 8) | data[2];
+  float hum = (raw_hum / 100.0);
+  printf("Il fait %f%\n", hum);
 }
--- tp-catie/src/exo4.hpp
+++ tp-catie/src/exo4.hpp
@@ -1,18 +1,26 @@
 #ifndef EXO4_HPP
 #define EXO4_HPP
 
+#include "bme280.h"
 #include "mbed.h"
 #include "rtos.h"
-#include "bme280.h"
 
 using namespace std::chrono;
 using namespace sixtron;
 
 #define PERIOD_MS 2000ms
+#define I2C_ADDRESS 0x76
+#define TEMP_REG 0xFA
+#define PRESS_REG 0xF7
+#define HUM_REG 0xF5
 
 extern BME280 bme280;
 extern I2C i2c;
 void read_temperature(BME280 bme280);
 void read_pressure(BME280 bme280);
 void read_humidity(BME280 bme280);
+
+void read_raw_temperature(I2C i2c);
+void read_raw_pressure(I2C i2c);
+void read_raw_humidity(I2C i2c);
 #endif
